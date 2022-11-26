5fbbadcf58658fd53d450ebf51db44d4a1dfc191
Fazal00007 <111645285+Fazal00007@users.noreply.github.com>
2022-11-26 15:57:30 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+HELP_IMG = ["https://telegra.ph/file/1fb45e04755ed05a86bf4.mp4"]
 
-START_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+START_IMG = ["https://telegra.ph/file/7ae3550b42d4ce4411dbd.mp4"]
 
 
-PHOTO = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+PHOTO = ["https://telegra.ph/file/cda323726765086630ff8.jpg"]
