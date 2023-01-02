8bedbfd1b1cdca7053a9a26ec6fcb9772f91d4aa
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-02 23:06:27 +0530

    Update __init__.py

--- Exon/modules/helper_funcs/__init__.py
+++ Exon/modules/helper_funcs/__init__.py
@@ -2,6 +2,50 @@ from asyncio import gather
 
 from Exon import aiohttpsession as session
 
+from typing import Callable
+
+from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
+
+from Exon import app as abishnoi, DRAGONS
+
+
+def can_change_info(func: Callable) -> Callable:
+    async def non_admin(_, message: Message):
+        if message.from_user.id in DRAGONS:
+            return await func(_, message)
+
+        check = await abishnoi.get_chat_member(message.chat.id, message.from_user.id)
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await message.reply_text("» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs.")
+
+        admin = (await abishnoi.get_chat_member(message.chat.id, message.from_user.id)).privileges
+        if admin.can_change_info:
+            return await func(_, message)
+        else:
+            return await message.reply_text("`ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ɪɴғᴏ.")
+
+    return non_admin
+
+
+def can_restrict(func: Callable) -> Callable:
+    async def non_admin(_, message: Message):
+        if message.from_user.id in DRAGONS:
+            return await func(_, message)
+
+        check = await abishnoi.get_chat_member(message.chat.id, message.from_user.id)
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await message.reply_text("» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs.")
+
+        admin = (await abishnoi.get_chat_member(message.chat.id, message.from_user.id)).privileges
+        if admin.can_restrict_members:
+            return await func(_, message)
+        else:
+            return await message.reply_text("`ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ʀᴇsᴛʀɪᴄᴛ users ɪɴ ᴛʜɪs ᴄʜᴀᴛ.")
+
+    return non_admin
+
+
 
 async def get(url: str, *args, **kwargs):
     async with session.get(url, *args, **kwargs) as resp:
