65437770ddfbfd83ab0f2b8bdb40e8cea202c7ea
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-02 09:37:03 +0530

    Update __init__.py

--- Exon/__init__.py
+++ Exon/__init__.py
@@ -84,7 +84,7 @@ asyncio.get_event_loop().run_until_complete(EXON_PTB.bot.initialize())
 
 # ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.EXON
+db = mongo.Config.DB_NAME
 
 try:
     client = MongoClient(MONGO_DB_URI)
