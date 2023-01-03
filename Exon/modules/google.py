6731d1dd69bc2678f38f131b4d9df22b858590cd
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-04 02:45:15 +0530

    Update google.py

--- Exon/modules/google.py
+++ Exon/modules/google.py
@@ -209,10 +209,13 @@ async def wiki(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else update.effective_message
     )
     res = ""
-    if msg == update.effective_message:
-        search = msg.text.split(" ", maxsplit=1)[1]
-    else:
-        search = msg.text
+    args = context.args
+
+    if not args:
+        return await update.effective_message.reply_text(
+            "ʏᴏᴜ sʜᴏᴜʟᴅ ɢɪᴠᴇ sᴏᴍᴇᴛʜɪɴɢ ᴛᴏ sᴇᴀʀᴄʜ ғᴏʀ "
+        )
+    search = " ".join(args)
     try:
         res = wikipedia.summary(search)
     except DisambiguationError as e:
