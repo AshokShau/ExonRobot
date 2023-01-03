54d96119e8c4e430797ac31508a802513ed122ea
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-03 16:12:03 +0530

    Update misc.py

--- Exon/modules/helper_funcs/misc.py
+++ Exon/modules/helper_funcs/misc.py
@@ -1,4 +1,5 @@
 from html import escape
+from math import ceil
 from typing import Dict, List
 
 from telegram import Bot, InlineKeyboardButton
@@ -46,12 +47,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({})".format(
-                        prefix,
-                        x.__mod_name__.lower(),
+                        prefix, x.__mod_name__.replace(" ", "_").lower()
                     ),
                 )
                 for x in module_dict.values()
-            ],
+            ]
         )
     else:
         modules = sorted(
@@ -59,21 +59,52 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
-                        prefix,
-                        chat,
-                        x.__mod_name__.lower(),
+                        prefix, chat, x.__mod_name__.replace(" ", "_").lower()
                     ),
                 )
                 for x in module_dict.values()
-            ],
+            ]
         )
 
-    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
-
-    round_num = len(modules) / 3
-    calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
+    for m in pairs:
+        for _ in m:
+            i += 1
+    if len(modules) - i == 1:
         pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
+        pairs.append(
+            (
+                modules[-2],
+                modules[-1],
+            )
+        )
+
+    COLUMN_SIZE = 4
+
+    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > COLUMN_SIZE:
+        pairs = pairs[modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "☜",
+                    callback_data="{}_prev({})".format(prefix, modulo_page),
+                ),
+                EqInlineKeyboardButton(
+                    "ɢᴏ ʜᴏᴍᴇ",
+                    callback_data="start_back",
+                ),
+                EqInlineKeyboardButton(
+                    "☞",
+                    callback_data="{}_next({})".format(prefix, modulo_page),
+                ),
+            )
+        ]
+
     return pairs
 
 
