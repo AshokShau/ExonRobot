b03ff9d1792bebb2537ea88bc70288a6ce2741ff
Abishnoi69 <1072003+Abishnoi69+@github.com>
2023-01-10 11:01:56 +0000

    ᴇxᴏɴ-ᴠ2.69

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     API_ID = int(getenv("API_ID", "13600724"))
     API_HASH = getenv("API_HASH", "ee59fd28d0d065c6b7d105082c6a0ba0")
-    TOKEN = getenv("TOKEN", None)  # ɢᴇᴛ ᴏɴᴇ ғʀᴏᴍ @BotFather [ᴅᴏɴ'ᴛ ᴀᴅᴅ ʜᴇᴀʀ ʙᴏᴛ ᴛᴏᴋᴇɴ ] 
+    TOKEN = getenv("TOKEN", None)  # ɢᴇᴛ ᴏɴᴇ ғʀᴏᴍ @BotFather [ᴅᴏɴ'ᴛ ᴀᴅᴅ ʜᴇᴀʀ ʙᴏᴛ ᴛᴏᴋᴇɴ ]
     OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
     OWNER_USERNAME = getenv("OWNER_USERNAME", None)  # ʏᴏᴜʀᴇ ᴛɢ ᴜsᴇʀɴᴀᴍᴇ ᴡɪᴛʜᴏᴜᴛ @
     SUPPORT_CHAT = getenv(
