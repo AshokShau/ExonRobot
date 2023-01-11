daf72e83eb8686dc0d4c8e00a1d81a00952265fd
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-11 19:38:19 +0530

    Create anon_admin.py

---
+++ Exon/modules/helper_funcs/anon_admin.py
@@ -0,0 +1,144 @@
+import functools
+from enum import Enum
+from telegram.constants import ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    ApplicationHandlerStop,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+from Exon import DEV_USERS, DRAGONS, application
+
+
+
+class AdminPerms(Enum):
+    CAN_RESTRICT_MEMBERS = "can_restrict_members"
+    CAN_PROMOTE_MEMBERS = "can_promote_members"
+    CAN_INVITE_USERS = "can_invite_users"
+    CAN_DELETE_MESSAGES = "can_delete_messages"
+    CAN_CHANGE_INFO = "can_change_info"
+    CAN_PIN_MESSAGES = "can_pin_messages"
+
+
+class ChatStatus(Enum):
+    CREATOR = "creator"
+    ADMIN = "administrator"
+
+
+anon_callbacks = {}
+anon_callback_messages = {}
+anon_users = {}
+
+
+def user_admin(permission: AdminPerms):
+    def wrapper(func):
+        @functools.wraps(func)
+        def awrapper(update: Update, context: CallbackContext, *args, **kwargs):
+            nonlocal permission
+            if update.effective_chat.type == "private":
+                return func(update, context, *args, **kwargs)
+            message = update.effective_message
+            is_anon = update.effective_message.sender_chat
+
+            if is_anon:
+                callback_id = (
+                    f"anoncb/{message.chat.id}/{message.message_id}/{permission.value}"
+                )
+                anon_callbacks[(message.chat.id, message.message_id)] = (
+                    (update, context),
+                    func,
+                )
+                anon_callback_messages[(message.chat.id, message.message_id)] = (
+                    message.reply_text(
+                        "sᴇᴇᴍs ʟɪᴋᴇ you're ᴀɴᴏɴʏᴍᴏᴜs, ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ᴘʀᴏᴠᴇ ʏᴏᴜʀ ɪᴅᴇɴᴛɪᴛʏ",
+                        reply_markup=InlineKeyboardMarkup(
+                            [
+                                [
+                                    InlineKeyboardButton(
+                                        text="ᴘʀᴏᴠᴇ ɪᴅᴇɴᴛɪᴛʏ", callback_data=callback_id
+                                    )
+                                ]
+                            ]
+                        ),
+                    )
+                ).message_id
+                # send message with callback f'anoncb{callback_id}'
+            else:
+                user_id = message.from_user.id
+                chat_id = message.chat.id
+                mem = context.bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+                if (
+                    getattr(mem, permission.value) is True
+                    or mem.status == "creator"
+                    or user_id in DRAGONS
+                ):
+                    return func(update, context, *args, **kwargs)
+                else:
+                    return message.reply_text(
+                        f"ʏᴏᴜ ʟᴀᴄᴋ ᴛʜᴇ ᴘᴇʀᴍɪssɪᴏɴ: `{permission.name}`",
+                        parse_mode=ParseMode.MARKDOWN,
+                    )
+
+        return awrapper
+
+    return wrapper
+
+
+async def anon_callback_handler1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    callback = upd.callback_query
+    perm = callback.data.split("/")[3]
+    chat_id = int(callback.data.split("/")[1])
+    message_id = int(callback.data.split("/")[2])
+    try:
+        mem = upd.effective_chat.get_member(user_id=callback.from_user.id)
+    except BaseException as e:
+        callback.answer(f"ᴇʀʀᴏʀ: {e}", show_alert=True)
+        return
+    if mem.status not in [ChatStatus.ADMIN.value, ChatStatus.CREATOR.value]:
+        callback.answer("ʏᴏᴜ'ʀᴇ ᴀʀᴇɴ'ᴛ  ᴀᴅᴍɪɴ.")
+        application.bot.delete_message(
+            chat_id, anon_callback_messages.pop((chat_id, message_id), None)
+        )
+        application.bot.send_message(
+            chat_id, "ʏᴏᴜ ʟᴀᴄᴋ ᴛʜᴇ ᴘᴇʀᴍɪssɪᴏɴs ʀᴇǫᴜɪʀᴇᴅ ғᴏʀ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ"
+        )
+    elif (
+        getattr(mem, perm) is True
+        or mem.status == "creator"
+        or mem.user.id in DEV_USERS
+    ):
+        cb = anon_callbacks.pop((chat_id, message_id), None)
+        if cb:
+            message_id = anon_callback_messages.pop((chat_id, message_id), None)
+            if message_id is not None:
+                application.bot.delete_message(chat_id, message_id)
+            return cb[1](cb[0][0], cb[0][1])
+    else:
+        callback.answer("ᴛʜɪs ɪsɴ'ᴛ ғᴏʀ ʏᴀ")
+
+
+def resolve_user(user, message_id, chat):
+    if user.id == 1087968824:
+        try:
+            uid = anon_users.pop((chat.id, message_id))
+            user = chat.get_member(uid).user
+        except KeyError:
+            return application.bot.edit_message_text(
+                chat.id,
+                message_id,
+                "ʏᴏᴜ'ʀᴇ ɴᴏᴡ ɪᴅᴇɴᴛɪғɪᴇᴅ ᴀs -: {}".format(user.first_name),
+            )
+        except BaseException as e:
+            return application.bot.edit_message_text(chat.id, message_id, f"ᴇʀʀᴏʀ: {e}")
+
+    else:
+        user = user
+    return user
+
+
+anon_callback_handler = CallbackQueryHandler(anon_callback_handler1, pattern=r"anoncb", block=False)
+application.add_handler(anon_callback_handler)
