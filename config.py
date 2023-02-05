b5e6c805fcd3b09db540459b4ca33ab8fb1c4d88
Abishnoi69 <1072003+Abishnoi69+@github.com>
2023-02-05 04:59:07 +0000

    Auto-Fix

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-import json
-import os
 from os import getenv
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 
 class Config(object):
@@ -13,21 +11,20 @@ class Config(object):
     API_ID = int(getenv("API_ID", 6))
     API_HASH = getenv("API_HASH", None)
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
-    STRING_SESSION= getenv("STRING_SESSION", None)
+    STRING_SESSION = getenv("STRING_SESSION", None)
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", None) 
+    TOKEN = getenv("TOKEN", None)
     OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M") 
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")  
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701")) 
-    MONGO_URI = getenv("MONGO_DB_URI") 
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
+    MONGO_URI = getenv("MONGO_DB_URI")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
-    DATABASE_URL = getenv("DATABASE_URL")  
+    DATABASE_URL = getenv("DATABASE_URL")
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
-
 
 
 class Production(Config):
@@ -36,12 +33,3 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
-
-
-
-
-    
-  
-  
-
-   
