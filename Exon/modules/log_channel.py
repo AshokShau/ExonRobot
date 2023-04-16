cd96c16099583e083dda979c365eec3f080236ff
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-04-16 17:23:38 +0530

    removed `logsettings` # fixes

--- Exon/modules/log_channel.py
+++ Exon/modules/log_channel.py
@@ -27,27 +27,26 @@ SOFTWARE.
 #     UPDATE   :- Abishnoi_bots
 #     GITHUB :- ABISHNOI69 ""
 
+
 from datetime import datetime
 from functools import wraps
 
 from telegram.ext import CallbackContext
-
 from Exon.modules.helper_funcs.decorators import Exoncallback, Exoncmd
 from Exon.modules.helper_funcs.misc import is_module_loaded
-
 from ..modules.helper_funcs.anonymous import AdminPerms, user_admin
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
 if is_module_loaded(FILENAME):
-    from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+    from telegram import ParseMode, Update
     from telegram.error import BadRequest, Unauthorized
-    from telegram.ext import JobQueue
+    from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
-
-    from Exon import EVENT_LOGS, LOGGER, dispatcher
     from Exon.modules.helper_funcs.chat_status import is_user_admin
     from Exon.modules.helper_funcs.chat_status import user_admin as u_admin
+    from Exon import EVENT_LOGS, LOGGER, dispatcher
+    from Exon.modules.helper_funcs.chat_status import user_admin
     from Exon.modules.sql import log_channel_sql as sql
 
     def loggable(func):
@@ -71,15 +70,8 @@ if is_module_loaded(FILENAME):
                 datetime_fmt = "%H:%M - %d-%m-%Y"
                 result += f"\n<b>Event Stamp</b>: <code>{datetime.utcnow().strftime(datetime_fmt)}</code>"
 
-                try:
-                    if message.chat.type == chat.SUPERGROUP:
-                        if message.chat.username:
-                            result += f'\n<b>Link:</b> <a href="https://t.me/{chat.username}/{message.message_id}">click here</a>'
-                        else:
-                            cid = str(chat.id).replace("-100", "")
-                            result += f'\n<b>Link:</b> <a href="https://t.me/c/{cid}/{message.message_id}">click here</a>'
-                except AttributeError:
-                    result += "\n<b>Link:</b> No link for manual actions."  # or just without the whole line
+                if message.chat.type == chat.SUPERGROUP and message.chat.username:
+                    result += f'\n<b>Link:</b> <a href="https://t.me/{chat.username}/{message.message_id}">click here</a>'
                 log_chat = sql.get_chat_log_channel(chat.id)
                 if log_chat:
                     send_log(context, log_chat, chat.id, result)
@@ -98,7 +90,7 @@ if is_module_loaded(FILENAME):
             if result:
                 datetime_fmt = "%H:%M - %d-%m-%Y"
                 result += "\n<b>Event Stamp</b>: <code>{}</code>".format(
-                    datetime.utcnow().strftime(datetime_fmt),
+                    datetime.utcnow().strftime(datetime_fmt)
                 )
 
                 if message.chat.type == chat.SUPERGROUP and message.chat.username:
@@ -125,8 +117,7 @@ if is_module_loaded(FILENAME):
         except BadRequest as excp:
             if excp.message == "Chat not found":
                 bot.send_message(
-                    orig_chat_id,
-                    "This log channel has been deleted - unsetting.",
+                    orig_chat_id, "This log channel has been deleted - unsetting."
                 )
                 sql.stop_chat_logging(orig_chat_id)
             else:
@@ -167,7 +158,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
         if chat.type == chat.CHANNEL:
             message.reply_text(
-                "Now, forward the /setlog to the group you want to tie this channel to!",
+                "Now, forward the /setlog to the group you want to tie this channel to!"
             )
 
         elif message.forward_from_chat:
@@ -175,9 +166,11 @@ if is_module_loaded(FILENAME):
             try:
                 message.delete()
             except BadRequest as excp:
-                if excp.message != "Message to delete not found":
+                if excp.message == "Message to delete not found":
+                    pass
+                else:
                     LOGGER.exception(
-                        "Error deleting message in log channel. Should work anyway though.",
+                        "Error deleting message in log channel. Should work anyway though."
                     )
 
             try:
@@ -198,7 +191,7 @@ if is_module_loaded(FILENAME):
                 "The steps to set a log channel are:\n"
                 " - add bot to the desired channel\n"
                 " - send /setlog to the channel\n"
-                " - forward the /setlog to the group\n",
+                " - forward the /setlog to the group\n"
             )
 
     @Exoncmd(command="unsetlog")
@@ -211,8 +204,7 @@ if is_module_loaded(FILENAME):
         log_channel = sql.stop_chat_logging(chat.id)
         if log_channel:
             bot.send_message(
-                log_channel,
-                f"Channel has been unlinked from {chat.title}",
+                log_channel, f"Channel has been unlinked from {chat.title}"
             )
             message.reply_text("Log channel has been un-set.")
 
@@ -220,7 +212,7 @@ if is_module_loaded(FILENAME):
             message.reply_text("No log channel has been set yet!")
 
     def __stats__():
-        return f"× {sql.num_logchannels()} log channels set."
+        return f"• {sql.num_logchannels()} ʟᴏɢ ᴄʜᴀɴɴᴇʟs sᴇᴛ."
 
     def __migrate__(old_chat_id, new_chat_id):
         sql.migrate_chat(old_chat_id, new_chat_id)
@@ -232,7 +224,13 @@ if is_module_loaded(FILENAME):
             return f"This group has all it's logs sent to: {escape_markdown(log_channel_info.title)} (`{log_channel}`)"
         return "No log channel is set for this group!"
 
-    __mod_name__ = "𝐋ᴏɢɢᴇʀ"
+    from Exon.modules.language import gs
+
+
+    def get_help(chat):
+        return gs(chat, "log_help")
+
+    __mod_name__ = "𝐋ᴏɢɢᴇʀs"
 
 else:
     # run anyway if module not loaded
@@ -241,88 +239,3 @@ else:
 
     def gloggable(func):
         return func
-
-
-@Exoncmd("logsettings")
-@user_admin(AdminPerms.CAN_CHANGE_INFO)
-def log_settings(update: Update, _: CallbackContext):
-    chat = update.effective_chat
-    chat_set = sql.get_chat_setting(chat_id=chat.id)
-    if not chat_set:
-        sql.set_chat_setting(
-            setting=sql.LogChannelSettings(chat.id, True, True, True, True, True)
-        )
-    btn = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(text="Warn", callback_data="log_tog_warn"),
-                InlineKeyboardButton(text="Action", callback_data="log_tog_act"),
-            ],
-            [
-                InlineKeyboardButton(text="Join", callback_data="log_tog_join"),
-                InlineKeyboardButton(text="Leave", callback_data="log_tog_leave"),
-            ],
-            [InlineKeyboardButton(text="Report", callback_data="log_tog_rep")],
-        ]
-    )
-    msg = update.effective_message
-    msg.reply_text("Toggle channel log settings", reply_markup=btn)
-
-
-from Exon.modules.sql import log_channel_sql as sql
-
-
-@Exoncallback(pattern=r"log_tog_.*")
-def log_setting_callback(update: Update, context: CallbackContext):
-    cb = update.callback_query
-    user = cb.from_user
-    chat = cb.message.chat
-    if not is_user_admin(update, user.id):
-        cb.answer("You aren't admin", show_alert=True)
-        return
-    setting = cb.data.replace("log_tog_", "")
-    chat_set = sql.get_chat_setting(chat_id=chat.id)
-    if not chat_set:
-        sql.set_chat_setting(
-            setting=sql.LogChannelSettings(chat.id, True, True, True, True, True)
-        )
-
-    t = sql.get_chat_setting(chat.id)
-    if setting == "warn":
-        r = t.toggle_warn()
-        cb.answer("Warning log set to {}".format(r))
-        return
-    if setting == "act":
-        r = t.toggle_action()
-        cb.answer("Action log set to {}".format(r))
-        return
-    if setting == "join":
-        r = t.toggle_joins()
-        cb.answer("Join log set to {}".format(r))
-        return
-    if setting == "leave":
-        r = t.toggle_leave()
-        cb.answer("Leave log set to {}".format(r))
-        return
-    if setting == "rep":
-        r = t.toggle_report()
-        cb.answer("Report log set to {}".format(r))
-        return
-
-    cb.answer("Now, What i need to do")
-
-
-__mod_name__ = "𝐋ᴏɢs "
-
-# ғᴏʀ ʜᴇʟᴘ ᴍᴇɴᴜ
-
-
-# """
-from Exon.modules.language import gs
-
-
-def get_help(chat):
-    return gs(chat, "log_help")
-
-
-# """
