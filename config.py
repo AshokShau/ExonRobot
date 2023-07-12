b030383eb14d3a1c9d91cec1f1c99a39c0279360
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-12 08:42:35 +0530

    v3

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import json
+import os
 from os import getenv
 
 from dotenv import load_dotenv
@@ -5,26 +7,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def get_user_list(config, key):
+    with open("{}/Exon/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
+
 class Config(object):
     LOGGER = True
-
-    API_ID = int(getenv("API_ID", 6))
-    API_HASH = getenv("API_HASH", None)
-    ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
-    STRING_SESSION = getenv("STRING_SESSION", None)
-    SPAMWATCH_API = None
     TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
+    START_IMG = getenv(
+        "START_IMG", "https://te.legra.ph/file/1298fa07d367674fba061.jpg"
+    )
+    OWNER_ID = int(getenv("OWNER_ID", "5938660179"))
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
-    MONGO_URI = getenv("MONGO_DB_URI")
-    REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
-    DATABASE_URL = getenv("DATABASE_URL")
-
-    # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+    EVENT_LOGS = int(getenv("EVENT_LOGS", "-1001819078701"))
+    MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+    DATABASE_URL = getenv("DATABASE_URL", None)
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
 
 
 class Production(Config):
