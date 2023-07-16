8a2cb248795c9fd965e2d7a3071d63e18432f16d
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-16 23:20:39 +0530

    Update Callback.py

--- Exon/modules/Callback.py
+++ Exon/modules/Callback.py
@@ -30,35 +30,10 @@ SOFTWARE.
 from pyrogram.types import CallbackQuery
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import CallbackQueryHandler
-
+from Abg.helpers.human_read import get_readable_time
 from Exon import BOT_NAME, OWNER_ID, OWNER_USERNAME, SUPPORT_CHAT
 from Exon import Abishnoi as pbot
 from Exon import dispatcher
-
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
-
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
 
 
 @pbot.on_callback_query()
