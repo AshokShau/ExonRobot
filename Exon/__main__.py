93b30f00760eb3e1160f2482d1b3dc0339525bb5
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-16 23:22:16 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -2,7 +2,7 @@ import importlib
 import re
 import time
 from sys import argv, version_info
-
+from Abg.helpers.human_read import get_readable_time
 from pyrogram import __version__ as pver
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram import __version__ as lver
@@ -35,29 +35,6 @@ from Exon.modules.helper_funcs.misc import paginate_modules
 from Exon.modules.language import gs
 
 
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
-
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
 
 
 PM_START_TEX = """
