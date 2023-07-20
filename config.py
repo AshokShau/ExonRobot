e781e6e05cd82be418e00d386acfe8b671e5309e
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-20 12:35:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,16 @@ class Config(object):
     API_ID = int(getenv("API_ID", 6))
     API_HASH = getenv("API_HASH", None)
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
-    STRING_SESSION = getenv("STRING_SESSION", None)
     SPAMWATCH_API = None
     TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
+    OWNER_ID = int(getenv("OWNER_ID", 5938660179))
     OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
     LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
-    MONGO_URI = getenv("MONGO_DB_URI")
+    MONGO_URI = getenv("MONGO_DB_URI", None)
     DB_NAME = getenv("DB_NAME", "ExonRobot")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
-    DATABASE_URL = getenv("DATABASE_URL")
+    DATABASE_URL = getenv("DATABASE_URL", None)
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
     if DATABASE_URL.startswith("postgres://"):
