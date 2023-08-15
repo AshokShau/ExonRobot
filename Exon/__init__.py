4472b45b4c02809da9a058db2b4b90a50025f7c7
Abishnoi69 <Abishnoi69@users.noreply.github.com>
2023-08-15 09:35:37 +0000

    Auto-Fix

--- Exon/__init__.py
+++ Exon/__init__.py
@@ -27,6 +27,7 @@ from telethon.sessions import MemorySession, StringSession
 
 StartTime = time.time()
 
+
 def get_user_list(__init__, key):
     with open("{}/Exon/{}".format(os.getcwd(), __init__), "r") as json_file:
         return json.load(json_file)[key]
