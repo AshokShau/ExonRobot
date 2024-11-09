e5ebcbb204888446107ff0bc3f1326d5bd0acde1
AshokShau <114943948+AshokShau@users.noreply.github.com>
2024-11-10 01:25:53 +0530

    make it more faster

--- Telegram/utils/extract_user.py
+++ Telegram/utils/extract_user.py
@@ -19,76 +19,37 @@ async def extract_user(
     reply_message = m.reply_to_message
     args = context.args
 
-    # 1. Retrieve user info using reply (if available)
     if reply_message and reply_message.from_user:
-        # Ignore forum topics in replies
-        if reply_message.forum_topic_created:
-            return None, None, None, None
+        user = reply_message.from_user
+        if user.id == 136817688:
+            user = reply_message.sender_chat
+        return user.id, user.first_name, user.username, m.text.split(None, 1)[1]
 
-        # Set initial values from the reply message
-        user_id = reply_message.from_user.id
-        user_first_name = reply_message.from_user.first_name
-        user_name = reply_message.from_user.username
-
-        # Handle a specific user ID case
-        if user_id == 136817688:
-            user_id = reply_message.sender_chat.id
-            user_first_name = reply_message.sender_chat.title
-            user_name = reply_message.sender_chat.username
-
-        # Check if there’s additional text after the command and return
-        res = m.text.split(None, 1)
-        return user_id, user_first_name, user_name, res[1] if len(res) > 1 else ""
-
-    # 2. Retrieve user info using text mentions
     entities = list(m.parse_entities([MessageEntity.TEXT_MENTION]))
-    ent = entities[0] if entities else None
-    if ent and ent.offset == len(m.text) - len(m.text.split(None, 1)[1]):
-        user_id = ent.user.id
-        user_first_name = ent.user.first_name
-        user_name = ent.user.username
-        text = m.text[ent.offset + ent.length :]
-        return user_id, user_first_name, user_name, text
+    if entities:
+        ent = entities[0]
+        if ent.offset == len(m.text) - len(m.text.split(None, 1)[1]):
+            return ent.user.id, ent.user.first_name, ent.user.username, m.text[ent.offset + ent.length :]
 
-    # 3. Retrieve user info using @username (remove '@' if present)
     if len(args) >= 1 and args[0].startswith("@"):
         user = await Users.get_user_info(args[0].lstrip("@"))
-        res = m.text.split(None, 2)
-        text = res[2] if len(res) >= 3 else None
         if user:
-            user_id = user["_id"]
-            user_first_name = user["name"]
-            user_name = user["username"]
-            return user_id, user_first_name, user_name, text
-        return None, None, None, text
+            return user["_id"], user["name"], user["username"], m.text.split(None, 2)[2]
+        else:
+            return None, None, None, m.text.split(None, 2)[2]
 
-    # 4. Retrieve user info using numeric user ID
     if len(args) >= 1 and args[0].isdigit():
         user_id = int(args[0])
-        res = m.text.split(None, 2)
-        text = res[2] if len(res) >= 3 else None
         user = await Users.get_user_info(user_id)
-
         if user:
-            user_first_name = user["name"]
-            user_name = user["username"]
-            return user_id, user_first_name, user_name, text
-
-        # Attempt to retrieve the user from Telegram if not in database
+            return user_id, user["name"], user["username"], m.text.split(None, 2)[2]
         try:
             user = await context.bot.get_chat(user_id)
-            return user.id, user.first_name, user.username, text
-        except BadRequest as exc:
-            if exc.message in ("User_id_invalid", "Chat not found"):
-                return None, None, None, text
-            raise exc
+            return user.id, user.first_name, user.username, m.text.split(None, 2)[2]
+        except BadRequest:
+            return None, None, None, None
 
-    res = m.text.split(None, 1)
     if sender:
-        # 5. Default to message sender if no user found in previous methods
-        user_id = m.from_user.id
-        user_first_name = m.from_user.first_name
-        user_name = m.from_user.username
-        return user_id, user_first_name, user_name, res[1] if len(res) > 1 else ""
+        return m.from_user.id, m.from_user.first_name, m.from_user.username, m.text.split(None, 1)[1]
 
-    return None, None, None, res[1] if len(res) > 1 else ""
+    return None, None, None, m.text.split(None, 1)[1]
