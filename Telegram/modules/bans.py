25456bab31ebf335b3b09b93bb05805500ce69a2
AshokShau <114943948+AshokShau@users.noreply.github.com>
2024-11-10 17:50:41 +0530

    mutes: done

--- Telegram/modules/bans.py
+++ Telegram/modules/bans.py
@@ -3,7 +3,7 @@ import re
 
 from ptbmod import Admins
 from ptbmod.decorator.cache import is_admin
-from telegram import Update, Message
+from telegram import Update, Message, ChatPermissions, ChatMember
 from telegram.ext import ContextTypes
 from telegram.helpers import mention_html
 
@@ -109,7 +109,7 @@ async def unban(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str | Non
     return
 
 
-@Cmd(command=["kick", "dkick", "tKick", "dtKick", "sKick", "stKick"])
+@Cmd(command=["kick", "dKick", "skick"])
 @Admins(permissions="can_restrict_members", is_both=True)
 async def kick(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Message:
     chat = update.effective_chat
@@ -146,3 +146,125 @@ async def kick(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Message:
         text += f"\n<b>Reason:</b> <code>{_reason}</code>"
     await m.reply_text(text)
 
+
+@Cmd(command=["mute", "dMute", "sMute", "tMute"])
+@Admins(permissions="can_restrict_members", is_both=True)
+async def mute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Message | None:
+    chat = update.effective_chat
+    m = update.effective_message
+    reply_msg = m.reply_to_message or m
+
+    user_id, user_first_name, user_name, _reason = await extract_user(
+        m, context
+    )
+    if (
+        not user_id
+        or user_id == context.bot.id
+        or await is_admin(chat.id, user_id)
+    ):
+        return await m.reply_text(
+            "I don't know who you're talking about, you need to specify a user."
+            if user_id is None
+            else "I can't mute myself."
+            if user_id == context.bot.id
+            else "What are you trying to do? You can't mute someone who's an admin."
+        )
+
+    delete = bool(m.text.startswith("/d") or m.text.startswith("!d"))
+    silent = bool(m.text.startswith("/s") or m.text.startswith("!s"))
+
+    match = re.match(r"([0-9]{1,})([dhms])*$", _reason) if _reason else None
+    if match:
+        ban_time = match.group(0)
+        reason = _reason[match.end():].strip()
+    else:
+        ban_time = "367d"
+        reason = _reason
+
+    try:
+        await chat.restrict_member(
+            user_id, until_date=create_time(ban_time), permissions=ChatPermissions()
+        )
+    except Exception as e:
+        await m.reply_text("Failed to mute;")
+        raise e
+
+    if delete:
+        await try_to_delete(reply_msg)
+    elif silent:
+        await asyncio.gather(try_to_delete(reply_msg), try_to_delete(m))
+        return None
+
+    text = f"<b>{mention_html(user_id, user_first_name)}</b> was muted for {tl_time(ban_time)}."
+    if reason:
+        text += f"\n<b>Reason:</b> <code>{reason}</code>"
+    await m.reply_text(text)
+    return None
+
+
+@Cmd(command=["unmute", "sUnmute"])
+@Admins(permissions="can_restrict_members", is_both=True)
+async def unmute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str | None:
+    chat = update.effective_chat
+    m = update.effective_message
+
+    reply_msg = m.reply_to_message or m
+
+    user_id, user_first_name, user_name, _reason = await extract_user(m, context)
+
+    if not user_id or user_id == context.bot.id or await is_admin(chat.id, user_id):
+        await m.reply_text(
+            "I don't know who you're talking about, you need to specify a user."
+            if user_id is None
+            else "look I can speak."
+            if user_id == context.bot.id
+            else "What are you trying to do? This user is admin so how possible is it to unmute them."
+        )
+        return None
+
+    member = await chat.get_member(int(user_id))
+    if member.status != ChatMember.RESTRICTED:
+        await m.reply_text("This user is not muted.")
+        return None
+
+    delete = bool(m.text.startswith("/d") or m.text.startswith("!d"))
+    silent = bool(m.text.startswith("/s") or m.text.startswith("!s"))
+
+    try:
+        await chat.restrict_member(user_id, permissions=ChatPermissions.all_permissions())
+    except Exception as e:
+        await m.reply_text("Failed to unmute;")
+        raise e
+    if delete:
+        await try_to_delete(reply_msg)
+    elif silent:
+        await asyncio.gather(try_to_delete(reply_msg), try_to_delete(m))
+        return
+
+    text = f"Fine, <b>{mention_html(user_id, user_first_name)}</b> can speak again."
+    if _reason:
+        text += f"\n<b>Reason:</b> <code>{_reason}</code>"
+    await m.reply_text(text)
+    return
+
+
+__mod_name__ = "Bans"
+
+__alt_name__ = ["ban", "unban", "mute", "unmute", "kick"]
+
+__help__ = """
+<b>Description :</b>
+Some people need to be publicly banned; spammers, annoyances, or just trolls.
+
+This module allows you to do that easily, by exposing some common actions, so everyone will see!
+────────────────────────
+
+<b>The Following Commands Are Admin Only :</b>
+
+๏ /ban X time reason<b>:</b> Bans the user replied to or tagged.
+๏ /sBan X time <b>:</b> Bans the user replied or tagged and deletes your message.
+๏ /dban X time reason<b>:</b> Bans the user replied and deletes their message.
+๏ /mute X time reason<b>:</b> Mutes the user replied to or tagged.
+๏ /sMute X time <b>:</b> Mutes the user replied or tagged and deletes your message.
+๏ /dMute X time reason<b>:</b> Mutes the user replied and deletes their message.
+"""
