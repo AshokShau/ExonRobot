c3177122a10c2174b67e6d41e3ada15d05c69b02
AshokShau <114943948+AshokShau@users.noreply.github.com>
2024-11-10 14:17:09 +0530

    afk: done

---
+++ Telegram/modules/afk.py
@@ -0,0 +1,169 @@
+from datetime import datetime
+from typing import Union, Optional
+
+from telegram import Update, MessageEntity
+from telegram.ext import ContextTypes, filters
+from telegram.helpers import mention_html
+
+from Telegram import Cmd, Msg
+from Telegram.database.afk_db import AfkDB
+from Telegram.database.users_db import Users
+from Telegram.utils.msg_types import get_welcome_type, Types, send_cmd
+
+
+def till_date(date):
+    form = "%Y-%m-%d %H:%M:%S"
+    return datetime.strptime(date, form)
+
+
+def get_hours(hour: str):
+    tim = hour.strip().split(":")
+    txt = ""
+    if int(tim[0]):
+        txt += f"{tim[0]} Hours "
+    if int(tim[1]):
+        txt += f"{tim[1]} Minutes "
+    if int(round(float(tim[2]))):
+        txt += f"{str(round(float(tim[2])))} Seconds"
+
+    return txt
+
+
+@Cmd(command=["afk", "brb"])
+async def afkUser(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    msg = update.effective_message
+
+    time = str(datetime.now()).rsplit(".", 1)[0]
+    reason, data_type, content = await get_welcome_type(msg)
+    await AfkDB(user.id).add_afk(time, reason, data_type, content)
+    reply = f"{mention_html(user.id, user.full_name)} is AFK."
+    if reason:
+        reply += f"\nReason: {reason}"
+    await msg.reply_text(reply)
+
+@Msg(filters.ChatType.GROUPS, group=2)
+async def afk_checker(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> Union[(Optional[int], Optional[str])]:
+    user = update.effective_user
+    chat = update.effective_chat
+    m = update.effective_message
+    bot, args = context.bot, context.args
+    repl = m.reply_to_message
+
+    rep_user_id = None
+    rep_user_name = None
+
+    if m.entities and m.parse_entities(
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION],
+    ):
+        entities = m.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION],
+        )
+
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                rep_user_id = ent.user.id
+                rep_user_name = ent.user.first_name
+
+            elif ent.type == MessageEntity.MENTION:
+                userInfo = await Users.get_user_info(
+                    m.text[ent.offset : ent.offset + ent.length]
+                )
+                try:
+                    rep_user_id = userInfo["_id"]
+                    rep_user_name = userInfo["name"]
+                except KeyError:
+                    return None
+                if not rep_user_id:
+                    return None
+
+    elif repl and repl.from_user:
+        rep_user_id = repl.from_user.id
+        rep_user_name = repl.from_user.first_name
+
+    is_afk = await AfkDB(user.id).check_afk()
+    is_rep_afk = await AfkDB(rep_user_id).check_afk() if rep_user_id else False
+    if is_rep_afk and rep_user_id != user:
+        con = await AfkDB.get_afk(rep_user_id)
+        time = till_date(con["time"])
+        media = con["media"]
+        media_type = con["media_type"]
+        tim_ = datetime.now() - time
+        tim_ = str(tim_).split(",")
+        tim = get_hours(tim_[-1])
+        tims = ""
+        if len(tim_) == 1:
+            tims = tim
+        elif len(tim_) == 2:
+            tims = f"{tim_[0]} {tim}"
+        reason = f"{rep_user_name} is AFK.\n<b>Since:</b> {tims}"
+        if con["reason"]:
+            reason += f"\n<b>Reason:</b> {con['reason']}"
+        txt = reason
+
+        if media_type == Types.TEXT:
+            await (await send_cmd(bot, media_type))(
+                chat.id,
+                txt,
+                reply_to_message_id=m.id,
+            )
+        elif media_type in (
+            Types.STICKER,
+            Types.VIDEO_NOTE,
+            Types.CONTACT,
+            Types.ANIMATED_STICKER,
+        ):
+            await (await send_cmd(bot, media_type))(
+                chat.id, media, reply_to_message_id=m.id
+            )
+        else:
+            await (await send_cmd(bot, media_type))(
+                chat.id, media, txt, reply_to_message_id=m.id
+            )
+
+    if is_afk:
+        txt = False
+        if m.text:
+            try:
+                txt = m.text.split(None, 1)[0]
+            except Exception:
+                raise
+
+        if txt and txt in ["!afk", "brb", "/afk"]:
+            return None
+
+        con = await AfkDB.get_afk(user.id)
+        time = till_date(con["time"])
+        tim_ = datetime.now() - time
+        tim_ = str(tim_).split(",")
+        tim = get_hours(tim_[-1])
+        tims = ""
+        if len(tim_) == 1:
+            tims = tim
+        elif len(tim_) == 2:
+            tims = f"{tim_[0]} {tim}"
+        txt = f"{user.mention_html()} is back online\nAFK for: {tims}"
+        if con["reason"]:
+            txt += f"\n<b>Reason:</b> {con['reason']}"
+        await m.reply_text(txt)
+
+        await AfkDB(user.id).remove_afk()
+    return
+
+__help__ = """
+Description:
+When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
+────────────────────────
+
+User Commands:
+• /afk: This will set you offline.
+• /afk reason: This will set you offline with a reason.
+• /afk replied to a sticker/photo: This will set you offline with an image or sticker.
+• /afk replied to a sticker/photo reason: This will set you AFK with an image and reason both.
+"""
+
+
+__mod_name__ = "AFK"
+__alt_name__ = ["afk", "away"]
---
+++ Telegram/utils/msg_types.py
@@ -0,0 +1,169 @@
+from enum import IntEnum, unique
+
+from telegram import Bot, Message
+
+
+@unique
+class Types(IntEnum):
+    TEXT = 1
+    DOCUMENT = 2
+    PHOTO = 3
+    VIDEO = 4
+    STICKER = 5
+    AUDIO = 6
+    VOICE = 7
+    VIDEO_NOTE = 8
+    ANIMATION = 9
+    ANIMATED_STICKER = 10
+    CONTACT = 11
+
+
+async def send_cmd(bot: Bot, msg_type: int):
+    get_format = {
+        Types.TEXT.value: bot.send_message,
+        Types.DOCUMENT.value: bot.send_document,
+        Types.PHOTO.value: bot.send_photo,
+        Types.VIDEO.value: bot.send_video,
+        Types.STICKER.value: bot.send_sticker,
+        Types.AUDIO.value: bot.send_audio,
+        Types.VOICE.value: bot.send_voice,
+        Types.VIDEO_NOTE.value: bot.send_video_note,
+        Types.ANIMATION.value: bot.send_animation,
+        Types.ANIMATED_STICKER.value: bot.send_sticker,
+        Types.CONTACT: bot.send_contact,
+    }
+    return get_format[msg_type]
+
+
+async def get_note_type(m: Message):
+    """Get type of note."""
+    if len(m.text.split()) <= 1:
+        return None, None, None, None
+
+    data_type = None
+    content = None
+    raw_text = m.text_html or m.caption_html
+    args = raw_text.split(None, 2)
+    note_name = args[1]
+
+    if len(args) >= 3:
+        text = args[2]
+        data_type = Types.TEXT
+
+    elif m.reply_to_message:
+        if m.reply_to_message.text:
+            text = m.reply_to_message.text_html
+        elif m.reply_to_message.caption:
+            text = m.reply_to_message.caption_html
+        else:
+            text = ""
+
+        if len(args) >= 2 and m.reply_to_message.text_html:  # not caption, text
+            data_type = Types.TEXT
+
+        elif m.reply_to_message.sticker:
+            content = m.reply_to_message.sticker.file_id
+            data_type = Types.STICKER
+
+        elif m.reply_to_message.document:
+            if m.reply_to_message.document.mime_type == "application/x-bad-tgsticker":
+                data_type = Types.ANIMATED_STICKER
+            else:
+                data_type = Types.DOCUMENT
+            content = m.reply_to_message.document.file_id
+
+        elif m.reply_to_message.photo:
+            content = m.reply_to_message.photo[-1].file_id  # last elem = best quality
+            data_type = Types.PHOTO
+
+        elif m.reply_to_message.audio:
+            content = m.reply_to_message.audio.file_id
+            data_type = Types.AUDIO
+
+        elif m.reply_to_message.voice:
+            content = m.reply_to_message.voice.file_id
+            data_type = Types.VOICE
+
+        elif m.reply_to_message.video:
+            content = m.reply_to_message.video.file_id
+            data_type = Types.VIDEO
+
+        elif m.reply_to_message.video_note:
+            content = m.reply_to_message.video_note.file_id
+            data_type = Types.VIDEO_NOTE
+
+        elif m.reply_to_message.animation:
+            content = m.reply_to_message.animation.file_id
+            data_type = Types.ANIMATION
+
+    else:
+        return None, None, None, None
+
+    return note_name, text, data_type, content
+
+
+async def get_welcome_type(m: Message):
+    data_type = None
+    content = None
+    raw_text = m.text_html or m.caption_html
+    args = raw_text.split(None, 1)
+
+    if not m.reply_to_message and m.text and len(m.text.split()) >= 2:
+        content = None
+        text = m.text.split(None, 1)[1]
+        data_type = Types.TEXT
+
+    elif m.reply_to_message:
+        if m.reply_to_message.text:
+            text = m.reply_to_message.text_html
+        elif m.reply_to_message.caption:
+            text = m.reply_to_message.caption_html
+        elif m.text and len(m.text.split()) >= 2:
+            text = m.text.split(None, 1)[1]
+        else:
+            text = ""
+
+        if len(args) >= 1 and m.reply_to_message.text_html:  # not caption, text
+            data_type = Types.TEXT
+
+        elif m.reply_to_message.sticker:
+            content = m.reply_to_message.sticker.file_id
+            data_type = Types.STICKER
+
+        elif m.reply_to_message.document:
+            if m.reply_to_message.document.mime_type == "application/x-bad-tgsticker":
+                data_type = Types.ANIMATED_STICKER
+            else:
+                data_type = Types.DOCUMENT
+            content = m.reply_to_message.document.file_id
+
+        elif m.reply_to_message.photo:
+            content = m.reply_to_message.photo[-1].file_id
+            data_type = Types.PHOTO
+
+        elif m.reply_to_message.audio:
+            content = m.reply_to_message.audio.file_id
+            data_type = Types.AUDIO
+
+        elif m.reply_to_message.voice:
+            content = m.reply_to_message.voice.file_id
+            data_type = Types.VOICE
+
+        elif m.reply_to_message.video:
+            content = m.reply_to_message.video.file_id
+            data_type = Types.VIDEO
+
+        elif m.reply_to_message.video_note:
+            content = m.reply_to_message.video_note.file_id
+            data_type = Types.VIDEO_NOTE
+
+        elif m.reply_to_message.animation:
+            content = m.reply_to_message.animation.file_id
+            data_type = Types.ANIMATION
+
+    else:
+        text = None
+        data_type = None
+        content = None
+
+    return text, data_type, content
